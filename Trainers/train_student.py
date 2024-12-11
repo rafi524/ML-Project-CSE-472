1ea482880e4478ee9bda6c18502cf7bf1802115f
Rakibul Hasan Rafi <92964753+rafi524@users.noreply.github.com>
2024-12-11 22:25:36 +0600

    student train  debug

--- Trainers/train_student.py
+++ Trainers/train_student.py
@@ -3,8 +3,10 @@ import json
 from Models.Student.Student import Student
 from DataManager.WordLoader import get_word_loader
 
+print("Loading graphemes dictionary...")
 with open('Graphemes/Extracted/graphemes_bw_bnhtrd_syn.json', 'r') as f:
     graphemes_dict = json.load(f)
+print("Graphemes dictionary loaded.")
 
 teacher_data = {
     'teacher_type': 'ResNet18',
@@ -14,10 +16,12 @@ teacher_data = {
 
 teacher_data = None
 variant = 'basic'
-epochs = 10
+epochs = 150
 use_attention = False
 
+print("Initializing Student model...")
 student  = Student(graphemes_dict, teacher_data=teacher_data, use_attention=use_attention, variant=variant)
+print("Student model initialized.")
 
 train_datasets = [
         {
@@ -34,9 +38,16 @@ val_datasets = [
        
     ]
 
+print("Loading training data...")
 train_loader, train_size = get_word_loader(train_datasets, augment=True)
+print(f"Training data loaded. Size: {train_size}")
+
+print("Loading validation data...")
 val_loader, val_size = get_word_loader(val_datasets, augment=False)
+print(f"Validation data loaded. Size: {val_size}")
 
 checkpoint_root='ML-Project-Files/Checkpoints'
 
+print("Starting training...")
 student.train(train_loader, val_loader, checkpoint_root=checkpoint_root, resume=False, epochs=epochs)
+print("Training completed.")
