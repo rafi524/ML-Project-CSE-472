2cc7e5c69e33288347055445c62f961600f3d1fd
Rakibul Hasan Rafi <92964753+rafi524@users.noreply.github.com>
2024-12-15 19:59:07 +0600

    val

--- Trainers/test_student.py
+++ Trainers/test_student.py
@@ -9,16 +9,10 @@ with open('Graphemes/Extracted/graphemes_bw_bnhtrd_syn.json', 'r') as f:
 student  = Student(graphemes_dict)
 
 test_datasets = [
-     
-        {
-            'img_dir': '/kaggle/working/curated_dataset/train/images',
-            'label_file_path': '/kaggle/working/curated_dataset/train/labels.csv',
-        },
-        {
-            'img_dir': '/kaggle/working/curated_dataset/val/images',
-            'label_file_path': '/kaggle/working/curated_dataset/val/labels.csv',
-        },
-    
+      {
+            'img_dir': '/kaggle/working/curated_dataset/test/images',
+            'label_file_path': '/kaggle/working/curated_dataset/test/labels.csv',
+        }
 
 ]
 
